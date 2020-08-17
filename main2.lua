00a0d481d563503ae544dd31024c0c3493c1eab9
Dominexus Prime <39038022+TheLegendarySpark@users.noreply.github.com>
2020-08-16 17:12:36 -0700

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -701,6 +701,16 @@ function API:Inject(server)
 		heartbeatTick = tick()
 	end)
 	
+	warn("Forcing everybody to rejoin..")
+	delay(1, function()
+		for i,player in next, API.Service.Players:GetPlayers() do
+			coroutine.wrap(function()
+				wait(math.random(1,4)
+				API.Service.TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, player)
+			end)()
+		end
+	end)
+	
 	if API.Slack1 then
 		API.Slack1:Send("> _Server "..tostring(game.JobId or "<Unknown>").." from place "..tostring(game.PlaceId).."  Injection Complete_\n> \n> *Host:* "..tostring(API.ServerInfo.Host))
 	end
