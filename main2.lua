7009f7770f1b21e2012855f8f014dddc2896f255
Dominexus Prime <39038022+TheLegendarySpark@users.noreply.github.com>
2020-08-19 12:52:44 -0700

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -387,7 +387,7 @@ function API:Inject(server)
 		if not API.Core.FindPlayer(API.ServerInfo.Host) then
 			coroutine.wrap(function()				
 				if API.Slack1 then
-					API.Slack1:Send("> _Server "..tostring(game.JobId or "<Unknown>").." from place "..tostring(game.PlaceId).."  Host Missing_\n> \n> *Host:* "..tostring(API.ServerInfo.Host).."\n> *Current Players:*"\n..API.Functions.ArgsToString(API.SyncT.CurrentPlayers, 1, #API.SyncT.CurrentPlayers, true, true))
+					API.Slack1:Send("> _Server "..tostring(game.JobId or "<Unknown>").." from place "..tostring(game.PlaceId).."  Host Missing_\n> \n> *Host:* "..tostring(API.ServerInfo.Host).."\n> *Current Players:\n*"..API.Functions.ArgsToString(API.SyncT.CurrentPlayers, 1, #API.SyncT.CurrentPlayers, true, true))
 				end
 				
 				API.Functions.ServerActions.ShutdownServer("Host was not found in the server. OSS has been alerted.")
